5c394d1958e1fd089e2495bd9fbcab198e0d8cd9
code-iddih <idinyamwaya@gmail.com>
2024-09-18 16:30:32 +0300

    Adds Color

--- mobi_services/main.py
+++ mobi_services/main.py
@@ -5,6 +5,7 @@ from fpdf import FPDF
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from models import User, Balance, Transaction, ActivityLog, Base
+from colors import *
 
 engine = create_engine('sqlite:///mobi_services.db')
 Session = sessionmaker(bind=engine)
@@ -12,7 +13,7 @@ session = Session()
 
 # Printing a welcoming message
 def welcome_message():
-    print("\nWelcome to MOBILE SERVICES\n")
+    print(f"\n{YELLOW}Welcome to MOBILE SERVICES{RESET}\n")
 
 # Log user activities
 def log_activity(user_id, action):
