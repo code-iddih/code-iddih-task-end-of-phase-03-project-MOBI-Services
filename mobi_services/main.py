4502bf35d89a32c1de9e892f3320d0e54efcd40b
code-iddih <idinyamwaya@gmail.com>
2024-09-18 09:45:00 +0300

    Adds Function to log User Activity

--- mobi_services/main.py
+++ mobi_services/main.py
@@ -8,5 +8,12 @@ engine = create_engine('sqlite:///mobi_services.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
+# Printing a wlecoming message
 def welcome_message():
     print("\nWelcome to MOBILE SERVICES\n")
+
+# Log user activities
+def log_activity(user_id, action):
+    new_activity = ActivityLog(user_id=user_id, action=action)
+    session.add(new_activity)
+    session.commit()
